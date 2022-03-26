8c6df607b8736096dc7d1d1cf01af4b242fdf300
tahuruzzoha-tuhin <tzz.tuhin@gmail.com>
2022-03-26 23:52:30 +0600

    modify one line

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,6 @@ const Header = () => {
         <div className="header-container">
             <h1>Choose your online course</h1>
             <p>Be the legend in your way to make knowledge</p>
-            {/* <h2>Total Budget This Year: $325M</h2> */}
         </div>
     );
 };
